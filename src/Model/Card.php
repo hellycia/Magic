1dc6e21c5c2e7b7586ac545d030f1670c3c17918
helene <helene.wattiaux@knplabs.com>
2019-11-29 10:09:21 +0100

    add feature card

--- src/Model/Card.php
+++ src/Model/Card.php
@@ -9,5 +9,11 @@ class Card
     public $cost;
     public $class;
     public $effect;
-    
+
+    public function __construct(string $name, int $cost, string $class) # quand on fait un new Card, ça lance construct
+    {
+        $this->name = $name;
+        $this->cost = $cost;
+        $this->class = $class;
+    }
 }
